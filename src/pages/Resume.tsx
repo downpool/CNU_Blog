7f955d4ca4211923963d4b5b417bab28ba652f52
donghyeon choi <downpool.choi@gmail.com>
2023-06-11 10:45:11 +0900

    .

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,9 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// import { Link } from 'react-router-dom';
 
 const Resume = () => {
   return (
     <div>
-      <h2>🌐대외활동🌐</h2>
+      <h2>WWDC23 학생 공모전 참가</h2>
+      <h3>SwiftUI를 이용함</h3>
+      <br />
+      <h2>수상</h2>
+      <h3>devday 2022-01 동상</h3>
     </div>
   );
 };
